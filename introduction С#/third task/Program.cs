3e14983a74c2b88923c838dfbf5b7774b9b6e023
[SashaPLS] <[playastaaa@mail.ru]>
2022-08-06 18:06:28 +0300

    third task

---
+++ introduction С#/third task/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int secondNumber = 2;
+if (number % secondNumber == 0)
+    {
+    Console.WriteLine("Число является чётным");
+    }
+else
+    {
+    Console.WriteLine("Введите другое число, это число нечётное");
+    }
+
+
+
