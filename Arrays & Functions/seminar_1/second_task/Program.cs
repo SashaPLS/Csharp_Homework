ceff2ce46db941b9073342151a0693be80bdb9b9
SashaPLS <playastaaa@mail.ru>
2022-08-14 22:59:44 +0300

    task2

---
+++ Arrays & Functions/seminar_1/second_task/Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine ("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 99 && number >1)
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
+else if (number > 100 && number < 1000)    
+    {
+        number = number % 100 / 10;
+        Console.WriteLine("Вторая цифра этого числа - " +number);
+    }
+else if (number > 1000 && number < 10000)    
+    {
+        number = number % 1000 / 100;
+        Console.WriteLine("Вторая цифра этого числа - " +number);
+    }
+else if (number > 10000 && number < 100000)    
+    {
+        number = number % 10000 / 1000;
+        Console.WriteLine("Вторая цифра этого числа - " +number);
+    }
