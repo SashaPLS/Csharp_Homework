fabbe5aad9779d5d3e09a16dd02fca7473114897
SashaPLS <playastaaa@mail.ru>
2022-08-15 00:02:12 +0300

    task1

---
+++ Arrays & Functions/seminar_2/task_1/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+string number = ReadInt("Введите пятизначное число для проверки: ");
+string firstNumber = Convert.ToString(number);
+
+if (firstNumber[0] == firstNumber[4] && firstNumber[1] == firstNumber[3])
+
+    {
+    Console.WriteLine("Число является палиндромом");
+    }
+else
+
+    Console.WriteLine("Число не является палиндромом");
+
+string ReadInt(string message)
+{
+    Console.Write(message);
+    return Console.ReadLine();
+}
