46bcc4861690bcd027ffca30b207403bd1f2ef97
SashaPLS <playastaaa@mail.ru>
2022-08-14 22:48:35 +0300

    first

---
+++ Arrays & Functions/seminar_1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа. 
+
+string number = ReadInt("Введите трехзначное число: ");
+
+Console.WriteLine("Вторая цифра этого числа ->" +number[1]);
+
+string ReadInt(string message)
+{
+    Console.Write(message);
+    return Console.ReadLine();
+}
