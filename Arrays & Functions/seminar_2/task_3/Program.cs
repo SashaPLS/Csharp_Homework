d24a0caba375576bf263882ea439e912d833a942
[SashaPLS] <[playastaaa@mail.ru]>
2022-08-16 00:29:13 +0300

    question

--- Arrays & Functions/seminar_2/task_3/Program.cs
+++ Arrays & Functions/seminar_2/task_3/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("Введите число: ");
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Введите число: ");
 int N = Convert.ToInt32(Console.ReadLine());
 int i = 1;
 while(i<=N)
