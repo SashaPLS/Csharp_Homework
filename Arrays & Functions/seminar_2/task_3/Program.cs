6c4f5b6102c82ce382e5fb4570760a7f2a604d0d
[SashaPLS] <[playastaaa@mail.ru]>
2022-08-16 00:26:54 +0300

    task_3

---
+++ Arrays & Functions/seminar_2/task_3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+while(i<=N)
+{
+    if(i == N)
+    {
+        Console.WriteLine(i*i*i);
+    }
+    else
+    {
+        Console.Write(i*i*i+ ", ");
+    }
+    i++;
+}
