d4d67a0df82f01ea558b224ed4e9262a3100a53c
[SashaPLS] <[playastaaa@mail.ru]>
2022-08-17 19:17:16 +0300

    okey

---
+++ Arrays & Functions/seminar_3/task_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+int numberA = ReadInt("Введите число А: ");
+int numberB = ReadInt("Введите число В: ");
+
+double powMethod = Math.Pow (numberA,numberB);
+
+Console.WriteLine("Число "+numberA+" в степени "+numberB+" равно: "+powMethod+"");
+
+int ReadInt(string message)
+{
+Console.Write(message);
+return Convert.ToInt32(Console.ReadLine());
+}
