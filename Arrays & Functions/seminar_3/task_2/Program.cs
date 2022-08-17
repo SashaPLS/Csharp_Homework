3ca309a320c91d3f1b99238ed6a00c43bf2c001c
[SashaPLS] <[playastaaa@mail.ru]>
2022-08-17 23:24:49 +0300

    add

---
+++ Arrays & Functions/seminar_3/task_2/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int number = ReadInt("Введите число: ");
+int temp = 0;
+int count = 0;
+int len = CountDigit(number);
+int CountDigit(int a)
+{
+    int c = 0;
+    while(a > 0)
+    {
+        a /= 10;
+        c++;
+    }
+    return c;
+}
+
+for (int i = 0; i < len; i++)
+{
+    temp = temp + number % 10;
+    number = number / 10;
+   
+}
+Console.WriteLine(temp);    
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
