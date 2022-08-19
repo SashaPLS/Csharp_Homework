bf18471a57b7587faa0ccd477cfba895e8c5f3cb
SashaPLS <playastaaa@mail.ru>
2022-08-19 12:57:27 +0300

    add

---
+++ Arrays & Functions/seminar_5/task_2/Program.cs
@@ -0,0 +1,38 @@
+﻿
+// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+Console.WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+int[] array = new int[size];
+
+FillArray(array);
+
+for (int i = 0; i < array.Length; i++)
+    if ( i % 2 != 0)
+    {
+        sum += array[i];
+    }
+
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine(sum);
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 100);
+    }
+}
