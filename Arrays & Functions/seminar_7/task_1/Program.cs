06356f9916807c6ac77b4be50fe33bd2e1755bb8
SashaPLS <playastaaa@mail.ru>
2022-08-26 15:05:19 +0300

    mdaaa

---
+++ Arrays & Functions/seminar_7/task_1/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+double rows = ReadInt("Введите количество строк: ");
+double columns = ReadInt("Введите количество столбцов: ");
+double[,] numbers = new double[rows, columns];
+FillArray2D(numbers);
+PrintArray2D(numbers);
+
+void FillArray2D(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().NextDouble() * 100;
+        }
+    }
+}
+
+void PrintArray2D(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+double ReadInt(string message)
+{
+    Console.Write(message);
+    return Double.Parse (Console.ReadLine());
+}
