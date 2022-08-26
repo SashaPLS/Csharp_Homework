89ae60c7d79f134f13109ef73cbeba894501e110
[SashaPLS] <[playastaaa@mail.ru]>
2022-08-26 16:56:46 +0300

    add

--- Arrays & Functions/seminar_6/task_2/Program.cs
+++ Arrays & Functions/seminar_6/task_2/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Напишите программу, которая найдёт точку пересечения двух прямых,
-// заданных уравнениями firstLine = k1 * x + b1, secondLine = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
@@ -7,13 +7,20 @@ double b1 = ReadInt("Введите число: ");
 double k1 = ReadInt("Введите число: ");
 double b2 = ReadInt("Введите число: ");
 double k2 = ReadInt("Введите число: ");
-double x = b1 - b2 / k2 - k1;
-double firstLine = k1 * x + b1;
-double secondLine = k2 * x + k2;
-double intersectionPoint = (k2 * b1 - k1 * b2) / (k2 - k1);
-double angleBetweenLines = k2 - k1 / 1 + k1 * k2;
 
-Console.WriteLine ($"Точка пересечения: {intersectionPoint}");
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 *x + b1;
+double y1 = k2 *x + b2;
+
+if(k1 == k2)
+{
+    Console.WriteLine($"Две прямые параллельны друг другу и не пересекаются");
+}
+else
+{
+Console.WriteLine($"Точка пересечения: ({x} ; {y})");
+Console.WriteLine($"Точка пересечения: ({x} ; {y1})");
+}
 
 double ReadInt(string message)
 {
