009a30dee650bd05e715b8327618934d99826a4f
[SashaPLS] <[playastaaa@mail.ru]>
2022-09-16 12:55:09 +0300

    a

---
+++ Arrays & Functions/seminar_8/task_4/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите
+//программу, которая будет находить строку с наименьшей суммой элементов.
+
+int m = 4;
+int n = 3;
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 100);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " "); ;
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] array = new int[m, n];
+
+FillArray(array);
+PrintArray(array);
+
+int mini = 0;
+int min = int.MaxValue;//для сравнения сумм максимально возможное значение
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    int sum = 0;
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        sum += array[i, j];
+    }
+    System.Console.Write(sum + " ");
+    
+    if (sum < min)
+    {
+        min = sum;
+        mini = i;
+    }
+}
+System.Console.WriteLine();
+System.Console.WriteLine($"Номер строки с наименьшей суммой элементов: {mini}");
