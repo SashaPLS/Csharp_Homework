7fe4317be0915d61819a7de646268fcbaceac8b8
[SashaPLS] <[playastaaa@mail.ru]>
2022-09-16 12:46:47 +0300

    a

---
+++ Arrays & Functions/seminar_8/task_3/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 60: Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить
+// массив, добавляя индексы каждого элемента.
+
+int a = 4;
+int b = 4;
+int c = 4;
+
+int[,,] array = new int[a,b,c];
+int[] save = new int[array.Length];
+bool exist = false;
+int count = 0;
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        for (int k = 0; k < array.GetLength(2); k++)
+        {
+            array[i, j, k] = new Random().Next(10, 100);
+            exist = false;
+            for (int l = 0; l < count; l++)
+            if(array[i,j,k] == save[l])
+            {
+                exist = true;
+                k--;
+                break;
+            }
+           if (exist == false)
+            {
+                Console.Write($"{array[i, j, k]}[{i},{j},{k}]" + " ");
+                save[count] = array[i,j,k];
+                count++; 
+            }
+        }
+    }
+    System.Console.WriteLine();
+}
